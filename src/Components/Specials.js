29b6a1246b99199551a9b152bb82379d6c41e775
David Cuevas Diaz <davidcuevasdiaz@Davids-Laptop.local>
2024-05-30 19:33:07 +0100

    add new css

--- src/Components/Specials.js
+++ src/Components/Specials.js
@@ -3,26 +3,49 @@ import '../App.css';
 
 function Specials() {
   return (
-    <div className="specials-container">
-      <div className="card-container">
-        <div className="card-title">Package 1</div>
-        <div className="card-description">Description</div>
-        <div className="card-price">€ 10.99</div>
-        <button className="card-button">Subscribe</button>
+    <section className="specials-section">
+      <div className="specials-header">
+        <h1>Special Deals</h1>
       </div>
-      <div className="card-container">
-        <div className="card-title">Package 2</div>
-        <div className="card-description">Description</div>
-        <div className="card-price">€ 10.99</div>
-        <button className="card-button">Subscribe</button>
+      <div className="specials-container">
+        <div className="card-container">
+          <div className="card-price">€10.99</div>
+          <div className="card-title">Package 1</div>
+          <ul>
+            <li>Feature 1</li>
+            <li>Feature 2</li>
+            <li>Feature 3</li>
+            <li>Feature 4</li>
+            <li>Feature 5</li>
+          </ul>
+          <button className="card-button">Subscribe</button>
+        </div>
+        <div className="card-container">
+          <div className="card-price">€20.99</div>
+          <div className="card-title">Package 2</div>
+          <ul>
+            <li>Feature 1</li>
+            <li>Feature 2</li>
+            <li>Feature 3</li>
+            <li>Feature 4</li>
+            <li>Feature 5</li>
+          </ul>
+          <button className="card-button">Subscribe</button>
+        </div>
+        <div className="card-container">
+          <div className="card-price">€30.99</div>
+          <div className="card-title">Package 3</div>
+          <ul>
+            <li>Feature 1</li>
+            <li>Feature 2</li>
+            <li>Feature 3</li>
+            <li>Feature 4</li>
+            <li>Feature 5</li>
+          </ul>
+          <button className="card-button">Subscribe</button>
+        </div>
       </div>
-      <div className="card-container">
-        <div className="card-title">Package 3</div>
-        <div className="card-description">Description</div>
-        <div className="card-price">€ 10.99</div>
-        <button className="card-button">Subscribe</button>
-      </div>
-    </div>
+    </section>
   );
 }
 
